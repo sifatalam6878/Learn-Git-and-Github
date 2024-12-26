47dcdb307fb9e5e252a58aa98d505a2cd2acddfc
sifatalam6878 <sifaterabbi20@gmail.com>
2024-12-26 21:31:05 +0600

    print hello world

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<ctype.h>
+#include<stdbool.h>
+#include<limits.h>
+#include<float.h>
+#include<time.h>
+
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
